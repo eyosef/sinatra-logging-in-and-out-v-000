d022e8827a4abc5b22c9a12476d25fee5852bbbb
Eden Mesfun <emesfun@gmail.com>
2018-04-04 13:04:26 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params["username"])
+    @user = User.find_by(username: params[:username])
     binding.pry
     if @user.password == params[:password]
       session[:user_id] = @user.id
