bca85848ca92dd626857cd5511227c1712244165
Eden Mesfun <emesfun@gmail.com>
2018-04-04 14:42:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by_id(session[:user_id])
     if @user == nil
       erb :error
-    else
+    elsif Helper.is_logged_in?(user)
       erb :account
     end
   end
