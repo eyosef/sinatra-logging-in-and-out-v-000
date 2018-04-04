5e906de11ad31ca631d071ee1dbbcd41d368b31a
Eden Mesfun <emesfun@gmail.com>
2018-04-04 13:31:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @user = User.find_by("#{session[:user_id]}")
+    @user = User.find_by(session[:user_id])
     binding.pry
     erb :account
   end
