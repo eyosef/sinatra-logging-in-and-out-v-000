99905157a1bd6c797afc8d6cd8dfc33eecf6600a
Eden Mesfun <emesfun@gmail.com>
2018-04-04 14:22:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by(username: params[:username])
     if @user == nil
       redirect to '/'
-    else 
+    else
       @user.password == params[:password]
       session[:user_id] = @user.id
       redirect to '/account'
